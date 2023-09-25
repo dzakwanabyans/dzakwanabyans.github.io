22ad42d6e2e357edbcfbb2bbdb38f0e7d2ce93fc
dzakwanabyans <dzekoneabyn25@gmail.com>
2023-09-25 15:34:57 +0700

    phoneview test

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 //     let header=document.querySelector('header');
 //     header.classList.toggle('sticky', window.scrollY > 100);
 // }
+
+const about = document.querySelector('#about');
+about.addEventListener('click', function(){
+    about.style.display=none;
+})

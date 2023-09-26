0cb9b39abea9c92e69c49c567d3b3f2ae7950691
dzakwanabyans <dzekoneabyn25@gmail.com>
2023-09-27 06:56:40 +0700

    navbar js

--- script.js
+++ script.js
@@ -2,8 +2,59 @@
 //     let header=document.querySelector('header');
 //     header.classList.toggle('sticky', window.scrollY > 100);
 // }
+const home = document.querySelector(".home");
+const about = document.querySelector(".about");
+const contact = document.querySelector(".contact");
 
-const about = document.querySelector('#about');
-about.addEventListener('click', function(){
-    about.style.display=none;
+
+function show(pr){
+    pr.style.display='';
+}
+
+function noShow(pr){
+    pr.style.display='none';
+}
+
+noShow(about);
+noShow(contact);
+
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", function(){
+    const homeClick = menu.querySelector("#home");
+    const aboutClick = menu.querySelector("#about");
+    const contactClick = menu.querySelector("#contact");
+    const portofolioClick = menu.querySelector("#portofolio");
+
+
+    homeClick.addEventListener("click", function(){
+        noShow(about);
+        noShow(contact);
+        show(home);
+        homeClick.classList.add("active");
+        contactClick.classList.remove("active");
+        aboutClick.classList.remove("active");
+    })
+
+    aboutClick.addEventListener("click", function(){
+        show(about);
+        noShow(contact);
+        noShow(home);
+        aboutClick.classList.add("active");
+        homeClick.classList.remove("active");
+        aboutClick.classList.remove("active");
+    })
+
+    contactClick.addEventListener("click", function(){
+        noShow(about);
+        show(contact);
+        noShow(home);
+        contactClick.classList.add("active");
+        homeClick.classList.remove("active");
+        aboutClick.classList.remove("active");
+    })
+
+
+    portofolioClick.addEventListener("click", function(){
+        [portofolioClick.innerHTML="Soon"]
+    })
 })
